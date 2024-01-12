8d4c84a263ca99bc2d6a2ea701c806286986ed75
MD20M <maj.donko@gmail.com>
2024-01-12 13:02:58 +0100

    Push2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import logo from './source_files/logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-function Nekaj(props){
+function Nekaj(){
+  const [tekst, setTekst] = useState(true);
+  const tekst1 = "Nekaj 1";
+  const tekst2 = "Nekaj 2";
+  return(
+    <>
+      <div id="neke"> Tekst v Nekaj funkciji</div>
+      <p>nekaj novega lala</p>
+      <button onClick={(e)=>{setTekst(!tekst)}}> klikni me</button>
+      <span>{tekst? tekst1 : tekst2}</span>
+    </>
+  )
+}
 
-  console.log(props)
+function Nekaj2(props){
 
   return(
-    <p>Nekaj {props[0]} x</p>
+      <p>{props["props"]%10==0 ? <p>Nekaj {props["props"]}x in DELJIVO S 10!!!!</p> : <p>Nekaj {props["props"]}x</p>}</p>  
   );
 }
 
@@ -28,7 +40,8 @@ function App() {
         >
           Learn React
         </a>
-        <Nekaj props={getNekaj}></Nekaj>
+        <Nekaj2 props={getNekaj}></Nekaj2>
+        <Nekaj></Nekaj>
         <button onClick={(e)=>setNekaj(getNekaj + 1)}>Button</button>
       </header>
     </div>
