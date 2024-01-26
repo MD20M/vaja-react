38e7067c526297263f5cac215f24e518a4e56ea6
MD20M <maj.donko@gmail.com>
2024-01-26 11:15:59 +0100

    NAv

--- src/App.js
+++ src/App.js
@@ -3,37 +3,19 @@ import './App.css';
 import { useState } from 'react';
 import { Nekaj } from './Fragmenti/Nekaj';
 import { Frag } from './Fragmenti/Frag';
+import Navbar from './Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
 
 
-function Nekaj2(props){
-
-  return(
-      <p>{props["props"]%10===0 ? <p>Nekaj {props["props"]}x in DELJIVO S 10!!!!</p> : <p>Nekaj {props["props"]}x</p>}</p>  
-  );
-}
 
 function App() {
-  const [getNekaj, setNekaj] = useState(1); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Nekaj></Nekaj>
-        <button onClick={(e)=>setNekaj(getNekaj + 1)}>Button</button>
-        <Frag></Frag>
-        <Nekaj2 props={getNekaj}></Nekaj2>
-      </header>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" component={<Nekaj/>}></Route>
+        <Route path="/frag" component={<Frag/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useRef, useState } from 'react';
+import { NavItem, NavbarText } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+export default function Navbar(){
+
+
+    return(
+      <>
+        <div>
+            <ul>
+                <li>
+                    <NavItem>
+                        <NavbarText>
+                            <Link to="/">Home</Link>
+                        </NavbarText>
+                    </NavItem>
+                </li>
+                <li>
+                    <NavItem>
+                        <NavbarText>
+                            <Link to="/frag">Frag</Link>
+                        </NavbarText>
+                    </NavItem>
+                </li>
+            </ul>
+            
+        </div>
+      </>
+    )
+  }
